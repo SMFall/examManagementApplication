22c550338093db918b4be1d5c47e091db9dc31dc
thomascastagno <castagnothomas@gmail.com>
2025-03-18 22:46:04 +0100

    Correction bug

--- examManagementApplication/src/main/java/org/amu/examManagement/repositories/ExamRepository.java
+++ examManagementApplication/src/main/java/org/amu/examManagement/repositories/ExamRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ExamRepository extends JpaRepository<Exam, Long> {
 
-    @Query(value = "SELECT * FROM EXAM JOIN USERS ON EXAM.USERS_ID = USERS.ID WHERE USERS.ID = ?1 AND USERS.ROLE = ?2 LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT e.* FROM EXAM e JOIN USERS u ON e.USERS_ID = u.ID WHERE u.ID = ?1 AND u.ROLE = ?2 LIMIT 1", nativeQuery = true)
     Exam findFirstByTeacherIdOrderByExamDateAsc(Long teacherId, String role);
 
 }
